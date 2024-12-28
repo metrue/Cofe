7010954aaae1c6e248c6426391823c8bc52d04a5
Minghe Huang <h.minghe@gmail.com>
2024-12-28 23:24:09 +0100

    wip: trying

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,6 +2,20 @@ import { PostContainer } from './component'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/lib/auth'
 import { createGitHubAPIClient } from '@/lib/client'
+import fs from 'fs'
+import path from 'path'
+
+export async function generateStaticParams() {
+  const blogDirectory = path.join(process.cwd(), 'data/blog')
+  const filenames = fs.readdirSync(blogDirectory)
+
+  // Generate params for each blog post based on filenames
+  const params = filenames.map((filename) => ({
+    id: encodeURIComponent(filename.replace(/\.md$/, '')), // Ensure non-ASCII slugs are encoded
+  }))
+
+  return params
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const session = await getServerSession(authOptions)
