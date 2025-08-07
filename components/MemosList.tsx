9cf94a4131f58273b14267d03d7909df27386cf8
Minghe <h.minghe@gmail.com>
2025-08-07 21:46:25 +0200

    fix(delete): enable the memo deletion (#25)

--- components/MemosList.tsx
+++ components/MemosList.tsx
@@ -27,6 +27,29 @@ export default function PublicMemosList({
     setFormattedMemos(formatted);
   }, [memos]);
 
+  const handleDelete = async (id: string) => {
+    try {
+      const response = await fetch('/api/github', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          action: 'deleteMemo',
+          id,
+        })
+      })
+      if (!response.ok) {
+        throw new Error('Failed to delete memo')
+      }
+
+      setFormattedMemos((preMemos) => preMemos.filter((memo) => memo.id !== id))
+    } catch (e) {
+      console.error(`error delete memo: ${e}`)
+    }
+  }
+
+
   return (
     <div className="max-w-2xl mx-auto p-4">
       <div className="grid grid-cols-2 md:grid-cols-2 gap-2">
@@ -35,7 +58,7 @@ export default function PublicMemosList({
             <MemoCard
               key={memo.id}
               memo={memo}
-              onDelete={() => {}}
+              onDelete={handleDelete}
               onEdit={() => {}}
             />
           ))}
