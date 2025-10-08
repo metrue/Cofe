427c9ec69e2d6674e1dea7fbb9f16048211a9801
Minghe <h.minghe@gmail.com>
2025-10-08 15:23:07 -0700

    Fix Reddit comments by passing client User-Agent to discussing package (#55)

--- components/BlogDiscussions.tsx
+++ components/BlogDiscussions.tsx
@@ -1,4 +1,5 @@
 import { DiscussionServer, type ExternalDiscussion } from 'discussing'
+import { headers } from 'next/headers'
 
 interface BlogDiscussionsProps {
   discussions?: ExternalDiscussion[]
@@ -13,11 +14,18 @@ export default async function BlogDiscussions({ discussions }: BlogDiscussionsPr
     return null
   }
 
+  // Get the client's User-Agent from the request headers
+  const headersList = headers()
+  const userAgent = headersList.get('user-agent') || 'Mozilla/5.0 (compatible; Blog Comment Fetcher)'
+
   return (
     <DiscussionServer
       discussions={discussions}
       className="mt-8 pt-6 border-t border-gray-100"
-      fetchOptions={{ cacheTimeout: 300 }}
+      fetchOptions={{ 
+        cacheTimeout: 300,
+        userAgent
+      }}
     />
   )
 }
